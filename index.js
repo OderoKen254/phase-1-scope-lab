af9625f5de130be4c47c638a00b0692b19fa317b
Kennedy Ouma Odero <kennedyrogouma222@gmail.com>
2025-03-17 00:02:56 +0300

    modified functions

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+console.log(customerName);
+
+
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+  }
+
+  
+  const leastFavoriteCustomer = 'initial value';
+
+  function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'new value';
+  }
+  
